43cf9ae8160788583c8fade7ce14266e15c8bbc7
sivalaxman007 <sivalaxman.mylapilli@constructn.ai>
2025-06-12 15:27:50 +0530

    thumbnail filter

--- src/modules/Images360/Images360.js
+++ src/modules/Images360/Images360.js
@@ -289,8 +289,8 @@ export class Images360 extends EventDispatcher{
 							//var sphereMaterial = new MeshBasicMaterial({ map: texture, side: DoubleSide });
                             //image360.texture = sphereMaterial;
 							texture.colorSpace = THREE.SRGBColorSpace;
-							texture.magFilter = THREE.NearestFilter;
-							texture.minFilter = THREE.LinearFilter;
+							// texture.magFilter = THREE.NearestFilter;
+							// texture.minFilter = THREE.LinearFilter;
 							image360.texture = texture;
 							resolved = true;
 							resolve(null);
